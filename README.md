1bad7afc2585fc028eaab876aa3d5e2abfc5a190
reallygoodprogrammer <milesdustin45@gmail.com>
2026-01-03 00:14:37 -0800

    switched to argparse, added manual signin step

--- README.md
+++ README.md
@@ -6,3 +6,21 @@ time the script is ran.
 
 Notice I made this run slow on purpose because I would prefer if my
 instagram account did not get banned.
+
+## usage
+
+```bash
+$ python3 befriend.py --help
+usage: befriend.py [-h] [-l LIKES] [-f FOLLOWS] [-s SESSION_FILE]
+
+automate following and liking on instagram
+
+options:
+  -h, --help            show this help message and exit
+  -l LIKES, --likes LIKES
+                        number of posts to like
+  -f FOLLOWS, --follows FOLLOWS
+                        number of suggested accounts to follow
+  -s SESSION_FILE, --session-file SESSION_FILE
+                        specify location of session file
+```
--- befriend.py
+++ befriend.py
@@ -1,70 +1,80 @@
 import sys
 import json
-import getopt
-from getpass import getpass
+import argparse
+import random
 from datetime import datetime
 from time import sleep
-from random import randint
 from random import uniform as rand
 from playwright.sync_api import sync_playwright
 
-USERNAME = "sap.love__"
-PASSWORD = ""
-SESS_FILE = ".befriend.sess"
 
-MAX_LIKES = randint(70,80)
-MAX_REC_FOLLOWS = 5
-
-# command line parsing and help menu
 def parse_opts():
-    if len(sys.argv) == 1: return
+    """
+    Parse arguments and return an `argparse` arguments object.
+    """
+    parser = argparse.ArgumentParser(
+        description="automate following and liking on instagram"
+    )
+    parser.add_argument(
+        "-l",
+        "--likes",
+        type=int,
+        default=random.randint(70, 80),
+        help="number of posts to like",
+    )
+    parser.add_argument(
+        "-f",
+        "--follows",
+        type=int,
+        default=5,
+        help="number of suggested accounts to follow",
+    )
+    parser.add_argument(
+        "-s",
+        "--session-file",
+        type=str,
+        default=".befriend.sess",
+        help="specify location of session file",
+    )
+    return parser.parse_args()
 
-    global MAX_LIKES
-    global MAX_REC_FOLLOWS
-    global SESS_FILE
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "l:f:s:hn")
-        for o, a in opts:
-            if o == "-l": MAX_LIKES = int(a)
-            elif o == "-f": MAX_REC_FOLLOWS = int(a)
-            elif o == "-s": SESS_FILE = str(a)
-            elif o == "-h": help()
-            else: logf(f"unidentified argument {o}")
-    except Exception as e:
-        logf(f"failed parsing arguments: {e}")
 
-def help():
-    print(f"usage: {sys.argv[0]} [options]")
-    print("befriend, an instagram account manager so you don't have to")
-    print("\t-l\t\tamount of posts to like on feed")
-    print("\t-f\t\tamount of accounts to follow from 'Suggested for you'")
-    print("\t-s\t\tsession file to load and save browser data to")
-    print("\t-h\t\tdisplay this handy help message")
-    sys.exit(0)
-
-
-# Log messaging
-def logf(msg : str):
-    log("FAILURE: "+msg)
+def logf(msg: str):
+    """
+    Log failure message to stderr and exit with status code `1`.
+    """
+    log("FAILURE: " + msg)
     sys.exit(1)
 
-def log(msg : str):
-    print("{0}: {1}".format(
-        datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        msg
-    ), file=sys.stderr)
+
+def log(msg: str):
+    """
+    Log message to stderr.
+    """
+    print(
+        "{0}: {1}".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), msg),
+        file=sys.stderr,
+    )
 
 
-# add random sleep periods
 def wait_small():
+    """
+    Sleep (blocking) for a period between 0.4 to 1.1 seconds.
+    """
     sleep(rand(0.4, 1.1))
 
+
 def wait_long():
+    """
+    Sleep (blocking) for a period between 4.5 to 11 seconds.
+    """
     sleep(rand(4.5, 11))
 
 
-# load and save context session data
-def sess_save(ctx, file_path=SESS_FILE):
+def sess_save(ctx, file_path: str):
+    """
+    Save session data to `file_path` for browser context `ctx`.
+    """
     log(f"saving session data to {file_path}...")
     try:
         with open(file_path, "w") as f:
@@ -72,23 +82,32 @@ def sess_save(ctx, file_path=SESS_FILE):
     except Exception as e:
         logf(str(e))
 
-def sess_load(ctx, file_path=SESS_FILE):
+
+def sess_load(ctx, file_path: str):
+    """
+    Load session data from `file_path` for browser context `ctx`.
+    """
     log(f"loading session data from {file_path}...")
     try:
         with open(file_path, "r") as f:
             cookies = json.load(f)
-            if not cookies: log("no session data in file")
-            else: ctx.add_cookies(cookies)
+            if not cookies:
+                log("no session data in file")
+            else:
+                ctx.add_cookies(cookies)
     except FileNotFoundError as e:
         log(f"session data not loaded: {e}")
     except Exception as e:
         logf(str(e))
 
 
-# check for login page, login to account if needed
 def login(ctx):
-    global USERNAME
-    global PASSWORD 
+    """
+    Create page from browser context `ctx` and visit instagram.
+
+    Check for a login page, and if so prompt user to enter login info manually, then
+    continue.
+    """
 
     page = ctx.new_page()
     page.goto("https://www.instagram.com/")
@@ -97,20 +116,10 @@ def login(ctx):
     pwd = page.get_by_label("Password")
 
     if usr.count() == 1 and pwd.count() == 1:
-        log("login page detected")
-        if PASSWORD == "": PASSWORD = getpass(f"enter password for {USERNAME}: ")
-        print("logging in ...")
-
-        usr.click()
-        wait_small()
-        usr.fill(USERNAME)
-        wait_small()
-        pwd.click()
-        wait_small()
-        pwd.fill(PASSWORD)
-        wait_small()
-
-        page.get_by_role("button", name="Log in", exact=True).click()
+        log(
+            "login page detected, please enter login info and then hit enter in terminal..."
+        )
+        input("")
         wait_long()
         page.wait_for_load_state("networkidle")
 
@@ -126,19 +135,26 @@ def login(ctx):
         page.wait_for_load_state("networkidle")
     else:
         log("login using session data successful :)")
-        
+
     return page
 
 
-def scroll_feed(page, mlikes : int):
+def scroll_feed(page, mlikes: int):
+    """
+    Scroll feed, liking `mlikes` number of posts that are not ads.
+    """
     log("liking posts in feed...")
     wait_small()
     i = 0
     for _ in range(0, mlikes):
-        post_locator = page.locator('article:has([role="button"]:has-text("Like"),'+\
-                '[role="button"]:has-text("More options"), [role="button"]:has-text("Save")'+
-                '):not(:has-text("Sponsored"))')
-        post_like_button = post_locator.get_by_role("button", name="Like", exact=True).first
+        post_locator = page.locator(
+            'article:has([role="button"]:has-text("Like"),'
+            + '[role="button"]:has-text("More options"), [role="button"]:has-text("Save")'
+            + '):not(:has-text("Sponsored"))'
+        )
+        post_like_button = post_locator.get_by_role(
+            "button", name="Like", exact=True
+        ).first
         post_like_button.scroll_into_view_if_needed()
         page.wait_for_load_state("networkidle")
         post_like_button.click()
@@ -147,7 +163,10 @@ def scroll_feed(page, mlikes : int):
     log(f"liked {i} posts from home feed")
 
 
-def follow_recs(page, mfollows : int):
+def follow_recs(page, mfollows: int):
+    """
+    Go to suggested users page and like `mfollows` number of users.
+    """
     page.get_by_role("link", name="See All", exact=True).click()
     wait_long()
     page.wait_for_load_state("networkidle")
@@ -156,42 +175,42 @@ def follow_recs(page, mfollows : int):
     for i in range(0, mfollows):
         profile.nth(i).click()
         wait_long()
-            
+
     page.get_by_role("link", name="Home Home").click()
     wait_long()
     page.wait_for_load_state("networkidle")
 
 
 def main():
-    global PASSWORD
-    global USERNAME
+    args = parse_opts()
 
-    parse_opts()
+    try:
+        with sync_playwright() as p:
+            log("loading browser")
+            browser = p.chromium.launch(headless=False)
+            context = browser.new_context()
 
-    with sync_playwright() as p:
-        log("loading browser")
-        browser = p.chromium.launch(headless=False)
-        context = browser.new_context()
+            log("loading session data from save file")
+            sess_load(context, args.session_file)
 
-        log("loading session data from save file")
-        sess_load(context, SESS_FILE)
+            log(
+                "following {0} users, liking {1} posts...".format(
+                    args.follows, args.likes
+                )
+            )
+            page = login(context)
+            if args.follows > 0:
+                follow_recs(page, args.follows)
+            if args.likes > 0:
+                scroll_feed(page, args.likes)
 
-        log("following {0} users, liking {1} posts...".format(MAX_REC_FOLLOWS, MAX_LIKES))
-
-        # login to instagram
-        page = login(context)
-        # follow/request some recommended profiles
-        if MAX_REC_FOLLOWS > 0:
-            follow_recs(page, MAX_REC_FOLLOWS)
-        # scroll feed and like some posts
-        if MAX_LIKES > 0:
-            scroll_feed(page, MAX_LIKES)
-
-        sess_save(context, SESS_FILE)
-        wait_long()
-        log("closing page")
-        page.close()
-    log("done :)")
+            sess_save(context, args.session_file)
+            wait_long()
+            page.close()
+        log("finished successfully :)")
+    except KeyboardInterrupt:
+        log("keyboard interrupt, saving session data...")
+        sess_save(context, args.session_file)
 
 
 if __name__ == "__main__":
